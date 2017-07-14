b1d00c0e093893a3574295e1ae720e5da6a0c3cb
Ruíz Arrieta Adolfo Agustín <aruiz@cofece.mx>
2017-07-14 12:56:51 -0500

    Geolocalización del estaciones de Gas LP en el pdf disponible en: https://www.gob.mx/cms/uploads/attachment/file/128552/Permisos_Estaciones_de_Servicio.pdf

--- codigo/geoloca_gaslp.r
+++ codigo/geoloca_gaslp.r
@@ -1,18 +1,25 @@
-install.packages("ggmap")
-
+library(readxl)
 library(ggmap)
-
-gas<-gas_geoloca
+#Leer permisos de gas LP
+gas<-read_excel("datos/Permisos_GASLP_pdf.xlsx")
 
 localiza<-data.frame()
 
-for(i in 1001:2420){
-      dir<-paste0(gas$Estado[i]," , ",gas$Municipio[i]," , ",gas$Dirección[i])
+for(i in 2001:3236){
+      dir<-paste0(gas$Calle[i],", ",gas$Municipio[i],", ",gas$Estado[i])
       dire<-geocode(dir)
-      geoloca<-data.frame( Permiso=gas$`No. Permiso`[i], Estado=gas$Estado[i], Municipio=gas$Municipio[i], Dir=gas$Dirección[i],latitud=dire$lat,longitud=dire$lon)
+      geoloca<-data.frame( Permiso= gas$`Número de Permiso`[i],
+                           Estado=gas$Estado[i], 
+                           Municipio=gas$Municipio[i],
+                           Marca=gas$`Marca Comercial`[i], 
+                           Razon=gas$`Razón social`[i],
+                           Dir=gas$Calle[i],
+                           latitud=dire$lat,
+                           longitud=dire$lon)
       localiza<-rbind(localiza,geoloca)
       print(i)
       
 }
 
-write.csv(localiza,"D:/Users/aruiz/Documents/Gas/localiza_gas.csv")
+#Escribir el csv en la carpeta datos
+write.csv(localiza,"datos/localiza_gas.csv")
