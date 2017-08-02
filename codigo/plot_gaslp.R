1d86189c6a5edbe27931c014f8d52e4909cdc649
Mancilla Chatú José Antonio <jamancilla@cofece.mx>
2017-08-02 09:19:41 -0500

    revision de codigo

--- codigo/plot_gaslp.R
+++ codigo/plot_gaslp.R
@@ -1,26 +1,29 @@
 library(leaflet)
 library(RColorBrewer)
+library(readxl)
 #leer datos
 geo_merge <- read_excel("datos/geo_merge.xlsx")
+
 #omitir blancos
 geo_merge_l<-na.omit(geo_merge)
 
 loca<- geo_merge_l[143:1210,]
-m<-leaflet(data = loca)
-#Definición de colores por Marca Comercial
+#Definici?n de colores por Marca Comercial
 colores<-loca$Marca
 
-s<-addTiles(m)
-#paleta de colores
 pal<-colorFactor(palette = rainbow(length(unique(colores))),
-                 domain = unique(colores), 
-                 levels = NULL, 
-                 ordered = FALSE, 
-                 na.color = "#808080", 
-                 alpha = FALSE, 
-                 reverse = FALSE)
+      domain = unique(colores), 
+      levels = NULL, 
+      ordered = FALSE, 
+      na.color = "#808080", 
+      alpha = FALSE, 
+      reverse = FALSE)
 
-#Definicion del mapa
-p<-addCircles(s,loca$longitud,loca$latitud, popup = c(loca$Razon_social), color=pal(colores))
+
 #ploteo del mapa
-p
+
+m<-leaflet(data = loca) %>%
+      addTiles() %>%
+      addCircles(~longitud,~latitud, popup = ~Razon_social, color=pal(colores))
+
+m
