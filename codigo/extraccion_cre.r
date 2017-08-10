ad49f5b8040b984e8e8f3c48e6c930f12aa55891
Mancilla Chatú José Antonio <jamancilla@cofece.mx>
2017-08-10 16:12:24 -0500

    comentario toño

--- codigo/extraccion_cre.r
+++ codigo/extraccion_cre.r
@@ -1,27 +1,34 @@
-#Garga de librería
+#Garga de librer?a
 library(XML)
+library(rvest)
 #creacion del data frame
 tabla_cre<-data.frame()
 #Tabla de datos de municipios
 N_est<-c(1:32)
-N_mun<-c(11,	5,	5,	11,	122,	67,	16,	38,	10,	39,	46,	81,	84,	125,	125,	113,	33,	20,	51,	570,	217,	18,	11,	58,	18,	72,	17,	43,	60,	212,	106,	58)
+N_mun<-c(11,5,5,11,122,67,16,38,10,39,46,81,84,125,125,113,33,20,51,570,217,18,11,58,18,72,17,43,60,212,106,58)
+
 rela<-data.frame(Estado=N_est,Municipio=N_mun)
-#Primer bucle de extracción por estado
-for(j in 1:1){
+#Primer bucle de extracci?n por estado
+for(j in 1:32){
+      # j <- 1
   #Segundo bucle de extraccion por municipio
-    for(k in 11:rela$Municipio[j]){
+    for(k in 1:rela$Municipio[j]){
+          # k <- 1
         mun<-as.character(1000+k)
         muni<-substr(mun,2,4)
         est<-as.character(100+j)
         esta<-substr(est,2,3)
-        cre<-read_html(paste0("http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=null&entidadId=",esta,"&municipioId=",muni))
+        Sys.sleep(60*2)
+        cre<- read_html(paste0("http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=1&entidadId=",esta,"&municipioId=",muni))
+        http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=1121&entidadId=02&municipioId=001
+        
         c<-as.character(cre)
         separa_1<-strsplit(c,":")
         separa_1
         separa_2<-strsplit(as.character(separa_1),"\"")
         separa_2
         r<-separa_2[[1]]
-        #Tercer bucle, estructuración de la informacion
+        #Tercer bucle, estructuraci?n de la informacion
         for(i in 0:5000){
             cre_tb<-data.frame(Razonsocial=r[17+88*i],
                                N_permiso=r[23+88*i],
@@ -30,7 +37,7 @@ for(j in 1:1){
                                Localidad=r[95+88*i],
                                Precio=r[56+88*i],
                                Fecha_aplica=r[61+88*i],
-                               Dirección=r[75+88*i])
+                               Direcci?n=r[75+88*i])
             tabla_cre<-rbind(tabla_cre,cre_tb)
       print(paste0("vamos en la ",i," del municipio ",k," del estado ",j))
 }
