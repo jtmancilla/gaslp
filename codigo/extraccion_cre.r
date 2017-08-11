3b2d115bddc3c46ef8743d19cae6bc9d24d165cc
Mancilla Chatú José Antonio <jamancilla@cofece.mx>
2017-08-11 11:35:57 -0500

    MEJORAS TONO

--- codigo/extraccion_cre.r
+++ codigo/extraccion_cre.r
@@ -1,48 +1,96 @@
-#Garga de librer?a
-library(XML)
-library(rvest)
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
 #creacion del data frame
 tabla_cre<-data.frame()
+autotanques <- data.frame()
+recipientes <- data.frame()
+
 #Tabla de datos de municipios
 N_est<-c(1:32)
 N_mun<-c(11,5,5,11,122,67,16,38,10,39,46,81,84,125,125,113,33,20,51,570,217,18,11,58,18,72,17,43,60,212,106,58)
 
 rela<-data.frame(Estado=N_est,Municipio=N_mun)
+
 #Primer bucle de extracci?n por estado
+
 for(j in 1:32){
+      
       # j <- 1
-  #Segundo bucle de extraccion por municipio
+  
+#Segundo bucle de extraccion por municipio
+
     for(k in 1:rela$Municipio[j]){
-          # k <- 1
+
+      # k <- 1
+        
         mun<-as.character(1000+k)
         muni<-substr(mun,2,4)
         est<-as.character(100+j)
         esta<-substr(est,2,3)
-        Sys.sleep(60*2)
-        cre<- read_html(paste0("http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=1&entidadId=",esta,"&municipioId=",muni))
-        http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=1121&entidadId=02&municipioId=001
         
-        c<-as.character(cre)
-        separa_1<-strsplit(c,":")
-        separa_1
-        separa_2<-strsplit(as.character(separa_1),"\"")
-        separa_2
-        r<-separa_2[[1]]
-        #Tercer bucle, estructuraci?n de la informacion
-        for(i in 0:5000){
-            cre_tb<-data.frame(Razonsocial=r[17+88*i],
-                               N_permiso=r[23+88*i],
-                               Estado=r[83+88*i],
-                               Municipio=r[89+88*i],
-                               Localidad=r[95+88*i],
-                               Precio=r[56+88*i],
-                               Fecha_aplica=r[61+88*i],
-                               Direcci?n=r[75+88*i])
-            tabla_cre<-rbind(tabla_cre,cre_tb)
-      print(paste0("vamos en la ",i," del municipio ",k," del estado ",j))
+        
+        Sys.sleep(60*2)
+        
+        repos <- GET(url = paste0("http://api-reportediario.cre.gob.mx/api/PlantaDistribucion/precio?localidadId=null&entidadId=",esta,"&municipioId=",muni))
+      
+      #  status_code(repos)
+       
+         repo_content <- content(repos)
+        
+        
+        repo_df <- lapply(repo_content$AutoTanques, function(x) {
+              df <- data_frame(
+                    RazonSocial             = x$RazonSocial,
+                    NumeroPermiso           = x$NumeroPermiso,
+                    EntidadFederativaId     = x$EntidadFederativaId,
+                    MunicipioId             = x$MunicipioId,
+                    LocalidadId             = x$LocalidadId,
+                    EsPermisionario         = x$EsPermisionario,
+                    MedioId               = x$MedioId,
+                    PrecioId              = x$PrecioId,
+                    Precio                = x$Precio,
+                    FechaAplicacion       = x$FechaAplicacion,
+                    Activo                = x$Activo,
+                    Direccion             = x$Direccion,
+                    EntidadFederativa     = x$EntidadFederativa,
+                    Municipio             = x$Municipio,
+                    Localidad             = x$Localidad)
+        }) %>% bind_rows()
+        
+        autotanques <- rbind(autotanques,repo_df)
+        
+        repo_df <- lapply(repo_content$Recipientes, function(x) {
+              df <- data_frame(
+                    RazonSocial             = x$RazonSocial,
+                    NumeroPermiso           = x$NumeroPermiso,
+                    EntidadFederativaId     = x$EntidadFederativaId,
+                    MunicipioId             = x$MunicipioId,
+                    LocalidadId             = x$LocalidadId,
+                    EsPermisionario         = x$EsPermisionario,
+                    MedioId               = x$MedioId,
+                    PrecioId              = x$PrecioId,
+                    CapacidadRecipiente   = x$CapacidadRecipiente, 
+                    Precio                = x$Precio,
+                    FechaAplicacion       = x$FechaAplicacion,
+                    Activo                = x$Activo,
+                    Direccion             = x$Direccion,
+                    EntidadFederativa     = x$EntidadFederativa,
+                    Municipio             = x$Municipio,
+                    Localidad             = x$Localidad)
+        }) %>% bind_rows()
+        
+        
+        recipientes <- rbind(recipientes,repo_df)
+        
+        
 }
 }
-}
-#Impresion de la tabla
-View(tabla_cre)
+
 
